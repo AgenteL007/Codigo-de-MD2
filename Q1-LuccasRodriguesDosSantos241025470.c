759ffcf18eed77c0666f51172649a45851176489
Luccas Rodrigues dos Santos <luccasrs2005@gmail.com>
2025-10-17 19:23:26 -0300

    first commit

---
+++ Q1-LuccasRodriguesDosSantos241025470.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()
+{
+    printf ("Codificador e Decodificador de Luccas Rodrigues dos Santos (241025470)\n");
+
+    int i;
+
+    printf ("Digite o valor de p (ou mande um EOF para encerrar): ");
+
+    while (scanf ("%d", &i) != EOF)
+    {
+        printf ("Digite o valor de q: ");
+        int q;
+        scanf ("%d", &q);
+
+        printf ("Digite o valor de z: ");
+        int z;
+        scanf ("%d", &z);
+
+    }
+
+    return 0;
+}
